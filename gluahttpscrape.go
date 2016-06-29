81881e6b6c1357719496b6f0330330cb22711f36
Felipe Cavalcanti <felipe.cavalcanti@tfgco.com>
2016-06-29 15:55:26 -0300

    findAttrByClass and findTextByClass

--- gluahttpscrape.go
+++ gluahttpscrape.go
@@ -1,9 +1,12 @@
 package gluahttpscrape
 
 import (
-	"fmt"
+	"strings"
 
+	"github.com/layeh/gopher-luar"
+	"github.com/yhat/scrape"
 	"github.com/yuin/gopher-lua"
+	"golang.org/x/net/html"
 )
 
 type httpScrapeModule struct{}
@@ -14,17 +17,56 @@ func NewHttpScrapeModule() *httpScrapeModule {
 
 func (h *httpScrapeModule) Loader(L *lua.LState) int {
 	mod := L.SetFuncs(L.NewTable(), map[string]lua.LGFunction{
-		"find_by_tag": h.findByTag,
-		//"findByClass": h.findByClass,
+		"find_attr_by_class": h.findAttrByClass,
+		"find_text_by_class": h.findTextByClass,
 		//"findById":    h.findById,
 	})
 	L.Push(mod)
 	return 1
 }
 
-func (h *httpScrapeModule) findByTag(L *lua.LState) int {
+func getMatcher(selector, value string) scrape.Matcher {
+	matcher := func(n *html.Node) bool {
+		return scrape.Attr(n, selector) == value
+	}
+	return matcher
+}
+
+func (h *httpScrapeModule) findAttrByClass(L *lua.LState) int {
 	body := L.ToString(1)
-	tag := L.ToString(2)
-	fmt.Printf("bode: %s, tag: %s", body, tag)
-	return 0
+	attr := L.ToString(2)
+	class := L.ToString(3)
+	root, err := html.Parse(strings.NewReader(body))
+	if err != nil {
+		L.Push(lua.LNil)
+		L.Push(lua.LString(err.Error()))
+		return 2
+	}
+	results := scrape.FindAll(root, getMatcher("class", class))
+	attrResults := []string{}
+	for _, result := range results {
+		attrResults = append(attrResults, scrape.Attr(result, attr))
+	}
+	L.Push(luar.New(L, attrResults))
+	L.Push(lua.LNil)
+	return 2
+}
+
+func (h *httpScrapeModule) findTextByClass(L *lua.LState) int {
+	body := L.ToString(1)
+	class := L.ToString(2)
+	root, err := html.Parse(strings.NewReader(body))
+	if err != nil {
+		L.Push(lua.LNil)
+		L.Push(lua.LString(err.Error()))
+		return 2
+	}
+	results := scrape.FindAll(root, getMatcher("class", class))
+	attrResults := []string{}
+	for _, result := range results {
+		attrResults = append(attrResults, scrape.Text(result))
+	}
+	L.Push(luar.New(L, attrResults))
+	L.Push(lua.LNil)
+	return 2
 }
---
+++ gluahttpscrape_test.go
@@ -0,0 +1,63 @@
+package gluahttpscrape
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/yuin/gopher-lua"
+)
+
+var httpBody = `<!DOCTYPE html><html><body><h1 class=\"testclass\" href=\"testhref\">My First Heading</h1><h1 class=\"testclass\" href=\"testhref2\">My First Heading</h1><p>My first paragraph.</p></body></html>`
+
+func TestFindAttrByClass(t *testing.T) {
+	if err := evalLua(t, `
+		local scrape = require("scrape")
+		response, error = scrape.find_attr_by_class("`+httpBody+`", "href", "testclass")
+		assert_equal("testhref", response[1])
+		assert_equal("testhref2", response[2])
+	`); err != nil {
+		t.Errorf("Failed to evaluate script: %s", err)
+	}
+}
+
+func TestFindTextByClass(t *testing.T) {
+	if err := evalLua(t, `
+		local scrape = require("scrape")
+		response, error = scrape.find_text_by_class("`+httpBody+`", "testclass")
+		assert_equal("My First Heading", response[1])
+		assert_equal("My First Heading", response[2])
+	`); err != nil {
+		t.Errorf("Failed to evaluate script: %s", err)
+	}
+}
+
+func evalLua(t *testing.T, script string) error {
+	L := lua.NewState()
+	defer L.Close()
+
+	L.PreloadModule("scrape", NewHttpScrapeModule().Loader)
+
+	L.SetGlobal("assert_equal", L.NewFunction(func(L *lua.LState) int {
+		expected := L.Get(1)
+		actual := L.Get(2)
+
+		if expected.Type() != actual.Type() || expected.String() != actual.String() {
+			t.Errorf("Expected %s %q, got %s %q", expected.Type(), expected, actual.Type(), actual)
+		}
+
+		return 0
+	}))
+
+	L.SetGlobal("assert_contains", L.NewFunction(func(L *lua.LState) int {
+		contains := L.Get(1)
+		actual := L.Get(2)
+
+		if !strings.Contains(actual.String(), contains.String()) {
+			t.Errorf("Expected %s %q contains %s %q", actual.Type(), actual, contains.Type(), contains)
+		}
+
+		return 0
+	}))
+
+	return L.DoString(script)
+}
